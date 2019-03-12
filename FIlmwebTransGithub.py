ae591d0ab16c86691c4d4fa19211ad765d90b826
rumburak56 <48485192+rumburak56@users.noreply.github.com>
2019-03-12 16:53:50 +0100

    Add files via upload

---
+++ FIlmwebTransGithub.py
@@ -0,0 +1,73 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+#variables
+n=1242 #length of the list
+
+
+f=open("input.txt", 'r')
+o=open("out.txt", 'w+')
+
+#setting the variable 'pom' for future checking
+my_url='https://www.filmweb.pl/films/search?q=Godfather'
+uClient = uReq(my_url)
+page_html=uClient.read()
+uClient.close()
+page_soup= soup(page_html, 'html.parser')
+pom=page_soup.find('div', class_='section__description')
+
+
+for j in range(n):
+    #making the url
+    a=f.readline()
+    inp=a.replace('\n', '')
+    key=(inp.replace(' ', '+'))
+    my_url='https://www.filmweb.pl/films/search?q='+key
+    print(my_url)
+
+    #downloading the page
+    uClient = uReq(my_url)
+    page_html=uClient.read()
+    uClient.close()
+    page_soup= soup(page_html, 'html.parser')
+
+    #searching for a film with searched title
+    tab=page_soup.find_all('div', class_="filmPreview__originalTitle")
+    index=-1
+    if(pom!=page_soup.find('div', class_='section__description')):
+        o.write(inp)
+        debug+=' :   '+inp
+    else:
+        for i in range(len(tab)):
+            if tab[i].string.lower()==inp.lower():
+                pomoc=tab[i].parent
+                pomoc=pomoc.div
+                pomoc=pomoc.a
+                pomoc=pomoc.h3
+                index=1
+                try: #trying to get the polish title if program found the english one
+                    debug+=' :   '+pomoc.string
+                    o.write(pomoc.string)
+                except:
+                    debug+=' :   '+inp
+                    o.write(inp)
+                break
+        if index==-1: #when it didnt find that film it will return the english title
+            debug+=' :   '+inp
+            o.write(inp)
+    o.write("\n")
+    print(debug)
+o.close()
+
+
+def sdadas():
+    if(index!=-1):
+        try:
+            o.write(page_soup.find_all("h3", class_="filmPreview__title")[index].string)
+            debug+=' :   '+page_soup.find_all("h3", class_="filmPreview__title")[index].string
+        except:
+            o.write(inp)
+            debug+=' :   '+inp
+    else:
+        o.write(inp)
+        debug+=' :   '+inp
